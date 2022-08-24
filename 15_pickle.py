e20ba1770f5f4a718771202d1325d6e00d3b7dee
levhyun <g88080997@gmail.com>
2022-08-24 22:13:15 +0900

    plus study 3

---
+++ 15_pickle.py
@@ -0,0 +1,10 @@
+import pickle
+
+p_file = open("p_file.pickle", "wb")
+file = {"1,2,3,4,5,6,7,8,9,10"}
+print(file)
+pickle.dump(file,p_file)
+p_file.close()
+
+with open("p_file.pickle", "rb") as p_file:
+    print(pickle.load(p_file))
