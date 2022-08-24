ece4e33e3129234adb653b33f1f981f3ca1cbe0a
levhyun <g88080997@gmail.com>
2022-08-24 16:57:29 +0900

    plus study

--- 4_repetition.py
+++ 4_repetition.py
@@ -1,19 +1,54 @@
-# 반복문 for
+# 반복문 for while
 
 list = ["1","2","3","4","5"]
 
 for i in list:
     print(i, end=" ")
 
-# 변수 int 정수, float 실수, ord 문자
+print("\n")
 
-a = 1.1
-b = ord('b')
-print("\n\n정수형 변수 a값 =",int(a)) #1
-print("\n실수형 변수 a값 =",float(a)) #1
-print("\n문자형 변수 a값 =",chr(b),"\n") #1
+drink = "커피"
+time = 5
+while time >= 1:
+    print("{0}준비까지 {1}초 남았습니다.".format(drink, time))
+    time -= 1
+    if time == 0:
+        print("\n커피가 준비되었습니다!")
+        break
+    else:
+        pass
 
-# 입력 input
+# 슬라이싱
 
-a=input() # 변수 a에 값을 입력 받음
-print(a)
+sl = "12345"
+print("1~4번째 수 : " + sl[0:4])
+
+# 리스트
+list = [1,2,3]
+print(list)
+
+list.append(4) # 리스트 제일 뒤에 4 추가
+print(list)
+
+list.extend([5,6]) # 리스트 제일 뒤에 5, 6 추가 (문자나 다른 변수의 리스트도 가능)
+print(list)
+
+list.insert(2,4) # 리스트 안에서 index 2번에 4를 넣고 원래있던 숫자와 뒤에있는 숫자들을 뒤로 한칸씩 옮긴다
+print(list)
+
+del list[0] # 리스트 값 삭제
+print(list)
+
+print(list.pop()) # 리스트에서 제일 뒤에 있는 값을 밖으로 내보낸후 내보낸 값 출력
+print(list)
+
+print(list.count(4)) # 리스트에서 4의 갯수 출력
+
+list2 = [5,4,3,2,1]
+list2.sort() # 리스트 오름차순으로 정렬
+print(list2)
+list2.reverse() # 리스트 뒤집기
+print(list2)
+
+list2.clear() # 리스트 안에 값 모두 삭제
+print(list2)
--- 5_function.py
+++ 5_function.py
@@ -39,3 +39,22 @@ print(int(random()*10)) # 0 ~ 10 미만의 임의의 값 생성
 #randrange = 범위지정 난수생성
 print(randrange(1,20)) # 1 ~ 20 미만의 임의의 값 생성
 print(randint(1,20)) # 1 ~ 20 이하의 임의의 값 생성
+
+# 문자영 처리 함수
+python = "Python is Amazing"
+print(python.lower()) # 변수명.lower() 는 변수명의 문자열 모두 소문자로 출력
+print(python.upper()) # 변수명.upper() 는 변수명의 문자열 모두 대문자로 출력
+print(python[0].isupper()) # 변수명[index번호].isupper() 는 변수명[index번호]에 해당하는 문자가 대문자이면 True 아니면 False 출력
+print(len(python)) # 변수 안에 문자열길이 출력
+print(python.replace("Python", "Mysql")) # 변수명.replace 는 함수괄호안에 첫번째 문자열이 변수안에 있다면 그문자열 자리에 두번째 문자열로 출력
+
+index = python.index("n") # 변수 안에 문자열에서 인덱스 0번부터 n을 찾으면 출력하고 못 찾으면 오류남
+print(index)
+index = python.index("n", index + 1) # 변수 안에 문자열에서 인덱스 0번부터 n을 찾고 n이 있다면 그 자리에서 +1한 자리에서 다시 찾은후 출력하고 못 찾으면 오류남
+print(index)
+
+print(python.find("n")) # .index 함수와 동일하지만 find함수는 n을 못 찾았을탠 -1출력
+print(python.find("Mysql")) # Mysql이란 단어가 python 변수안에 문자열에선 없어서 -1 출력
+
+print(python.count("n")) # 변수안에 문자열중 n의 갯수 출력
+
